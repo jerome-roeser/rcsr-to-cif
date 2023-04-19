492a4ed5a9654562fbedbf3fbaabfd213bc722bd
Jérôme Roeser <jerome.roeser@gmail.com>
2023-04-19 11:52:27 +0200

    edit greek character with utf code

--- cif_to_esi.py
+++ cif_to_esi.py
@@ -14,14 +14,7 @@ for p in path.iterdir():
         symmetry = f"Space Group: {cif[data]['_symmetry_space_group_name_H-M']} ({cif[data]['_symmetry_Int_Tables_number']})"
         crystal_system = cif[data]['_symmetry_cell_setting']
         cell_lenghts = f"a = {cif[data]['_cell_length_a']} Å, b = {cif[data]['_cell_length_b']} Å, c = {cif[data]['_cell_length_c']} Å"
-        cell_angles = f"a = b = {90.00} °, g = {120.00} °"
-        
-        # df_1 = pd.DataFrame({data_name: [symmetry, cell_lenghts, cell_angles]}) 
-        info =f"""
-        {symmetry}
-        {cell_lenghts}
-        {cell_angles}
-        """
+        cell_angles = f"\u03B1 = \u03B2 = {90.00} °, \u03B3 = {120.00} °"
         
         header = pd.MultiIndex.from_arrays([[data_name, '', '', '', ''], 
                                             [symmetry, '', '', '', ''],
@@ -34,19 +27,5 @@ for p in path.iterdir():
                             'y': cif[data]['_atom_site_fract_y'],
                             'z': cif[data]['_atom_site_fract_z']
                             })
-        # template_file = pd.read_excel('output\_template.xlsx')
         df_2.columns = header
-        # final = pd.concat([df_1,df_2])
-        # final.to_excel(f'output/{data_name}.xlsx')
         df_2.to_excel(f'output/{data_name}.xlsx')
-        
-        
-
-
-# filename = 'output\PD-29-14_Pawley.cif'
-# template = 'output\_template.xlsx'
-
-
-        
-# with open(filename, 'r') as f:
-#     a = f.readlines()
