a656d5ba830146b8dfed8b19bb866d616618164b
Jérôme Roeser <jerome.roeser@gmail.com>
2023-04-19 11:23:38 +0200

    edit header

--- cif_to_esi.py
+++ cif_to_esi.py
@@ -10,22 +10,36 @@ for p in path.iterdir():
             cif = ReadCif(f)
             
         data = cif.visible_keys[0]
-        lattice_parameter = []
+        data_name = p.stem
         symmetry = f"Space Group: {cif[data]['_symmetry_space_group_name_H-M']} ({cif[data]['_symmetry_Int_Tables_number']})"
         crystal_system = cif[data]['_symmetry_cell_setting']
         cell_lenghts = f"a = {cif[data]['_cell_length_a']} Å, b = {cif[data]['_cell_length_b']} Å, c = {cif[data]['_cell_length_c']} Å"
         cell_angles = f"a = b = {90.00} °, g = {120.00} °"
         
-        df_1 = pd.DataFrame({p.stem: [symmetry, cell_lenghts, cell_angles]})   
+        # df_1 = pd.DataFrame({data_name: [symmetry, cell_lenghts, cell_angles]}) 
+        info =f"""
+        {symmetry}
+        {cell_lenghts}
+        {cell_angles}
+        """
+        
+        header = pd.MultiIndex.from_arrays([[data_name, '', '', '', ''], 
+                                            [symmetry, '', '', '', ''],
+                                            [cell_lenghts, '', '', '', ''],
+                                            [cell_angles, '', '', '', ''],
+                                            ['Atom label', 'Atom type', 'x', 'y', 'z']])
         df_2 = pd.DataFrame({'Atom label': cif[data]['_atom_site_label'],
                             'Atom type' : cif[data]['_atom_site_type_symbol'],
                             'x' : cif[data]['_atom_site_fract_x'],
                             'y': cif[data]['_atom_site_fract_y'],
                             'z': cif[data]['_atom_site_fract_z']
                             })
-        final = pd.concat([df_1,df_2])
-        final.to_excel(f'output/{p.stem}.xlsx')
-        p.stem
+        # template_file = pd.read_excel('output\_template.xlsx')
+        df_2.columns = header
+        # final = pd.concat([df_1,df_2])
+        # final.to_excel(f'output/{data_name}.xlsx')
+        df_2.to_excel(f'output/{data_name}.xlsx')
+        
         
 
 
