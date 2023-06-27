0dbc03a81ef5a86a05cf1cbbb6a35d6d7af950e2
jerome-roeser <jerome.roeser@gmail.com>
2023-06-27 14:07:25 +0200

    fix origin problem in symmetry operations scraping

--- scrape_symmops.py
+++ scrape_symmops.py
@@ -2,17 +2,49 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+#TODO: for tetragonal and upwards replace with origin-2 (test status of a.htm sufix )
 # url_prefix = 'http://img.chem.ucl.ac.uk/sgp/large/'
 url_prefix = 'http://img.chem.ucl.ac.uk/sgp/medium/'
 
 url_sufixes = ['az3.htm', 'bz3.htm', 'cz3.htm', 'dz3.htm', 'ez3.htm', 'fz3.htm']
-symmops = {}
-symmops_a = {}
 
-for i in range(1,231):
-    key = str(i)
-    symmops_a[key] = []
-    for url_sufix in url_sufixes:
+# symmops_origin_1 = {}
+# symmops_origin_2 = {}
+symmops_all = {}
+
+def scrape_all_suffixes():
+    for i in range(1,231):
+        key = str(i)
+        symmops_all[key] = []
+        for url_sufix in url_sufixes:
+            # key = str(i) + '-' + url_sufix[0]
+            if i < 10:
+                url = url_prefix + '00' + str(i) + url_sufix
+            elif i < 100:
+                url = url_prefix + '0' + str(i) + url_sufix
+            else:
+                url = url_prefix + str(i) + url_sufix
+            try: 
+                r = requests.get(url)
+                if r.status_code == 200:
+                    soup = BeautifulSoup(r.content)
+                    symmops_all[key].append({url_sufix[0] : soup.pre.text.splitlines()})
+                    # symmops[key] = {url_sufix:soup.pre.text.splitlines()}
+                else:
+                    continue
+            except ConnectionError:
+                print('damn...')
+        
+    # symmops['227-b'] = soup.pre.text.splitlines()
+    with open('symmops.json', 'w') as f:
+        f.write(json.dumps(symmops_all))
+        
+def scrape_origin_1():
+    symmops_origin_1 = {}
+    for i in range(1,231):
+        key = str(i)
+        symmops_origin_1[key] = []
+        url_sufix = 'az3.htm'
         # key = str(i) + '-' + url_sufix[0]
         if i < 10:
             url = url_prefix + '00' + str(i) + url_sufix
@@ -24,13 +56,72 @@ for i in range(1,231):
             r = requests.get(url)
             if r.status_code == 200:
                 soup = BeautifulSoup(r.content)
-                symmops_a[key].append({url_sufix[0] : soup.pre.text.splitlines()})
+                symmops_origin_1[key].append({'origin-1' : soup.pre.text.splitlines()})
                 # symmops[key] = {url_sufix:soup.pre.text.splitlines()}
             else:
                 continue
         except ConnectionError:
             print('damn...')
+        
+    return symmops_origin_1
+  
+def scrape_origin_2(space_group__origin_2_list):
+    symmops_origin_2 = {}
+    for group in space_group__origin_2_list:
+        key = str(group)
+        symmops_origin_2[key] = []
+        url_sufix = 'bz3.htm'
+        # key = str(i) + '-' + url_sufix[0]
+        if group < 10:
+            url = url_prefix + '00' + str(group) + url_sufix
+        elif group < 100:
+            url = url_prefix + '0' + str(group) + url_sufix
+        else:
+            url = url_prefix + str(group) + url_sufix
+        try: 
+            r = requests.get(url)
+            if r.status_code == 200:
+                soup = BeautifulSoup(r.content)
+                symmops_origin_2[key].append({'origin-2' : soup.pre.text.splitlines()})
+                # symmops[key] = {url_sufix:soup.pre.text.splitlines()}
+            else:
+                continue
+        except ConnectionError:
+            print('damn...')
+    return symmops_origin_2
+        
+def find_origin_2_point_groups():
+    space_group_list = []
+    for i in range(1,231):
+        url_sufix = 'a.htm'
+        
+        if i < 10:
+            url = url_prefix + '00' + str(i) + url_sufix
+        elif i < 100:
+            url = url_prefix + '0' + str(i) + url_sufix
+        else:
+            url = url_prefix + str(i) + url_sufix
+        try: 
+            r = requests.get(url)
+            if r.status_code == 200:
+                space_group_list.append(i)
+            else:
+                continue
+        except ConnectionError:
+            print('damn...')
+        
+    return space_group_list
+
+def make_hybrid_symmops():
+    symmops = symmops_origin_1
+    for key in symmops_origin_2.keys(): 
+        symmops[key] = symmops_origin_2[key]
+    with open('symmops_final.json', 'w') as f:
+        f.write(json.dumps(symmops))
+    return symmops
     
-# symmops['227-b'] = soup.pre.text.splitlines()
-with open('symmops.json', 'w') as f:
-    f.write(json.dumps(symmops_a))
+    
+origin_2_list = find_origin_2_point_groups()
+symmops_origin_1 = scrape_origin_1()
+symmops_origin_2 = scrape_origin_2(origin_2_list)
+symmops = make_hybrid_symmops()
--- test_symmops.py
+++ test_symmops.py
@@ -15,14 +15,36 @@ from collections import Counter
 with open('symmops.json', 'rb') as f: 
     symmops = json.load(f)
 
-for i in range(0, 7):
-    l = [key for key, value in symmops.items() if len(value) == i]
-    print(len(l), l)
-    ids = []
-    for op in l:
-        for j in range(i):
-            ids.append([str(key) for key in symmops[op][j].keys()][0])
-    element_count = Counter(ids)
-    for element, count in element_count.items():
-        print(f"{element}: {count}")
-    
+# check origin occurrence
+def check_origin_reference():
+    for i in range(1, 7):
+        l = [key for key, value in symmops.items() if len(value) == i]
+        print(f"there are {len(l)} space groups with {i} different origins:")
+        ids = []
+        for op in l:
+            for j in range(i):
+                ids.append([str(key) for key in symmops[op][j].keys()][0])
+        element_count = Counter(ids)
+        for element, count in element_count.items():
+            print(f"\t{element}: {count}")
+        print('\n')
+        
+# i want to find the space groups with origin a and d 
+# and print if their sym op are equal
+def check_2_origin_point_groups():
+    l = [key for key, value in symmops.items() if len(value) == 2]
+    first_keys = []
+    second_keys = []
+    for op in l: 
+        key = [str(key) for key in symmops[op][1].keys()][0]
+        value_a = [value for key, value in symmops[op][0].items()]
+        value_d = [value for key, value in symmops[op][1].items()]
+        if key == 'b' and value_d != value_a:
+            point_group = SpaceGroup.from_int_number(int(op))
+            # print(point_group.full_symbol)
+            print(point_group.int_number, point_group.full_symbol, value_a == value_d)
+            print()
+        first_keys.append(([str(key) for key in symmops[op][0].keys()][0]))
+        second_keys.append(([str(key) for key in symmops[op][1].keys()][0]))
+
+check_2_origin_point_groups()
